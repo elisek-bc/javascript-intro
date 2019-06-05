bb45df0c0269ee73c08add58d058c30611368d98
elisek-bc <elisekppns@gmail.com>
2019-06-05 10:35:43 +0200

    adjusted write 05 ex

--- 07. RFW/03-Write-It/05/js/script.js
+++ 07. RFW/03-Write-It/05/js/script.js
@@ -32,3 +32,35 @@ const extras = [
     title : "Supplement Atmos"
   }
 ];
+
+const extraNames = extras.map((extra) => extra.title);
+const extraPrices = extras.map((extra) => extra.price);
+
+// create all combinations of fees and extras
+extraPrices.forEach((extra, i) => {
+  const sum = [];
+  sum.push(`${fees[0].title}: ` + (extra + fees[0].price));
+  sum.push(`${fees[1].title}: ` + (extra + fees[1].price));
+  sum.push(`${fees[2].title}: ` + (extra + fees[2].price));
+  console.log(sum);
+
+  // place items in list
+  const content = document.getElementById("content");
+
+  const text = document.createElement('h3');
+  console.log(i)
+    text.innerHTML = extraNames[i] //only the index part corresponding to the current forEach index of prices;
+    content.appendChild(text);
+  
+  const list = document.createElement('ul');
+  content.appendChild(list);
+
+  sum.forEach((data)=> {
+    const item = document.createElement('li');
+    item.innerHTML = data;
+    list.appendChild(item);
+  });
+  });
+
+
+
